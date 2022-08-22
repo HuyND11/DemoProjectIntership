a15e15e9b264ad04ff17eb6201348fd876541426
Nguyen Duc Huy <huy@pnvl109.pn.com.vn>
2022-08-22 11:14:52 +0700

    Edit spacing and responsive

--- Assets/JS/swiperJs.js
+++ Assets/JS/swiperJs.js
@@ -1,4 +1,10 @@
 var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
